cb1d6e962a0d040bf91e98de092c3f1a06667fc0
David Yi <1678014307@qq.com>
2025-10-08 20:58:43 +0800

    test js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,32 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", ()=> {
+    const mySrc = myImage.getAttribute("src");
+    if(mySrc === "images/test2.png"){
+        myImage.setAttribute("src", "images/test.png");
+    } else {
+        myImage.setAttribute("src", "images/test2.png");
+    }
+});
+
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcom to my world, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcom to my world, ${myName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
